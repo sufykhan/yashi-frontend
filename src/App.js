926ce9248574ad2ff0a0655f731da936d3b6c4f6
Sufyan <57655074+sufykhan@users.noreply.github.com>
2021-08-30 01:34:41 +0530

    Basic setup and Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Homescreen from "./Screens/Homescreen";
+import { Route } from "react-router-dom";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" exact>
+            <Homescreen />
+          </Route>
+        </Container>
+      </main>
+    </>
   );
-}
+};
 
 export default App;
+
---
+++ src/Screens/Homescreen.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Homescreen = () => {
+    return (
+        <div>
+            we start here
+        </div>
+    )
+}
+
+export default Homescreen
